0ba5ecad4c92d3a7fda7a6aca9c2b9e2dce74077
euhiduwu <18291854252@163.com>
2018-12-05 14:01:52 +0800

    修改userName

--- src/main/java/com/springboot/first/start/StartApplication.java
+++ src/main/java/com/springboot/first/start/StartApplication.java
@@ -18,7 +18,7 @@ public class StartApplication {
     String home(HttpServletRequest request) {
         String userName = "ctrip";
         Integer count = 100;
-        return "欢迎" + userName + "， 这是您的第" + count + "次登陆";
+        return "欢迎您" + userName + "， 这是您的第" + count + "次登陆";
     }
 
 	public static void main(String[] args) {
